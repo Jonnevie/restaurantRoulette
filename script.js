905496a64515a1e70898994c59680882fc395a4f
Jonnevie <56157123+Jonnevie@users.noreply.github.com>
2022-07-28 23:35:06 +1000

    image skew fix

---
+++ script.js
@@ -0,0 +1,18 @@
+// var myHeaders = new Headers();
+// myHeaders.append("Authorization", "Bearer 44RoJIuUtUZIYrH064l1n9MVbwjBjvRxDGjtkfJgnV5-J66COdSh7b-cKGOto_kviLyPuE0fDqkEnTiI2xiRzqDHYqYaUqsbYX6cUsKOxZYnUpHk8uSw2sHO-m_iYnYx");
+
+var requestOptions = {
+  method: 'GET',
+  headers: {
+    'Access-Control-Allow-Origin' : 'http://localhost"5500',
+    Authorization : 'Bearer 44RoJIuUtUZIYrH064l1n9MVbwjBjvRxDGjtkfJgnV5-J66COdSh7b-cKGOto_kviLyPuE0fDqkEnTiI2xiRzqDHYqYaUqsbYX6cUsKOxZYnUpHk8uSw2sHO-m_iYnYx'
+  },
+  redirect: 'follow'
+};
+
+fetch("https://api.yelp.com/v3/businesses/search?term=chinese&location=melbourne", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+  
