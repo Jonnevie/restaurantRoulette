ce50bc02961ad075ca3f845c566da66eec874fde
Jonnevie <56157123+Jonnevie@users.noreply.github.com>
2022-07-31 01:58:37 +1000

    all 3 cards shuffling

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let requestOptions = {
   redirect: "follow",
 };
 let fetchedRestaurant = [];
+console.log(fetchedRestaurant);
 let randomIndex = 0;
 
 //getting input values
@@ -20,11 +21,14 @@ let shootBtn = document.getElementById("shoot");
 shootBtn.addEventListener("click", function () {
   userInput = inputType.value;
   userLocation = inputLocation.value;
-  restaurants.fetchRestaurants().then(printRestaurant());
-  // .then((fetchedRestaurant = []));
+  //   restaurants.fetchRestaurants().then
+  let cardWarpper = document.getElementById("cardWrapper");
+  setTimeout(() => {
+    cardWarpper.classList.add("active");
+  }, 2000);
+  printRestaurant();
 });
 
-// var randomRestaurant = "None Found";
 let restaurants = {
   fetchRestaurants: async function () {
     const response = await fetch(
@@ -32,12 +36,11 @@ let restaurants = {
         userInput +
         "&location=" +
         userLocation +
-        "",
+        "&limit=50",
       requestOptions
     );
     const json = await response.json();
     fetchedRestaurant.push(json.businesses);
-    // console.log(fetchedRestaurant[0].length);
     randomIndex = Math.floor(Math.random() * fetchedRestaurant[0].length);
     return fetchedRestaurant[0][randomIndex];
   },
@@ -51,6 +54,26 @@ let addressCityState1 = document.getElementById("addressCityState-1");
 let addressCountry1 = document.getElementById("addressCountry-1");
 let phone1 = document.getElementById("phone1");
 let directions1 = document.getElementById("directions1");
+
+let name2 = document.getElementById("name2");
+let img2 = document.getElementById("img2");
+let paraText2 = document.getElementById("paraText2");
+let address22 = document.getElementById("address2-2");
+let address32 = document.getElementById("address3-2");
+let addressCityState2 = document.getElementById("addressCityState-2");
+let addressCountry2 = document.getElementById("addressCountry-2");
+let phone2 = document.getElementById("phone2");
+let directions2 = document.getElementById("directions2");
+
+let name3 = document.getElementById("name3");
+let img3 = document.getElementById("img3");
+let paraText3 = document.getElementById("paraText3");
+let address23 = document.getElementById("address2-3");
+let address33 = document.getElementById("address3-3");
+let addressCityState3 = document.getElementById("addressCityState-3");
+let addressCountry3 = document.getElementById("addressCountry-3");
+let phone3 = document.getElementById("phone3");
+let directions3 = document.getElementById("directions3");
 
 const printRestaurant = () => {
   restaurants.fetchRestaurants().then((restaurant) => {
@@ -73,7 +96,6 @@ const printRestaurant = () => {
         ? (address31.innerHTML = restaurant.location.address3)
         : (address31.style.display = "none")
     );
-
     addressCityState1.innerHTML = restaurant.location.city;
     addressCityState1.innerHTML += ", " + restaurant.location.state;
     addressCountry1.innerHTML = restaurant.location.country;
@@ -81,17 +103,57 @@ const printRestaurant = () => {
     phone1.innerHTML = restaurant.phone;
     directions1.href = restaurant.url;
   });
+  restaurants.fetchRestaurants().then((restaurant) => {
+    // randomRestaurant = restaurantName;
+    name2.innerHTML = restaurant.name;
+    if (restaurant.image_url ? (img2.src = restaurant.image_url) : img2);
+    if (
+      restaurant.location.address1
+        ? (paraText2.innerHTML = restaurant.location.address1)
+        : (paraText2.style.display = "none")
+    );
+    if (
+      restaurant.location.address2
+        ? (address22.innerHTML = restaurant.location.address2)
+        : (address22.style.display = "none")
+    );
+
+    if (
+      restaurant.location.address3
+        ? (address32.innerHTML = restaurant.location.address3)
+        : (address32.style.display = "none")
+    );
+    addressCityState2.innerHTML = restaurant.location.city;
+    addressCityState2.innerHTML += ", " + restaurant.location.state;
+    addressCountry2.innerHTML = restaurant.location.country;
+    dist2.innerHTML = (restaurant.distance / 1000).toFixed(0) + "km";
+    phone2.innerHTML = restaurant.phone;
+    directions2.href = restaurant.url;
+  });
+  restaurants.fetchRestaurants().then((restaurant) => {
+    name3.innerHTML = restaurant.name;
+    if (restaurant.image_url ? (img3.src = restaurant.image_url) : img3);
+    if (
+      restaurant.location.address1
+        ? (paraText3.innerHTML = restaurant.location.address1)
+        : (paraText3.style.display = "none")
+    );
+    if (
+      restaurant.location.address2
+        ? (address23.innerHTML = restaurant.location.address2)
+        : (address23.style.display = "none")
+    );
+
+    if (
+      restaurant.location.address3
+        ? (address33.innerHTML = restaurant.location.address3)
+        : (address33.style.display = "none")
+    );
+    addressCityState3.innerHTML = restaurant.location.city;
+    addressCityState3.innerHTML += ", " + restaurant.location.state;
+    addressCountry3.innerHTML = restaurant.location.country;
+    dist3.innerHTML = (restaurant.distance / 1000).toFixed(0) + "km";
+    phone3.innerHTML = restaurant.phone;
+    directions3.href = restaurant.url;
+  });
 };
-
-// const printRestaurant = async () => {
-//   const restaurantName = await restaurants.fetchRestaurants();
-//   return restaurantName;
-// };
-
-// function restaurantPick() {
-//   name1.innerHTML = randomRestaurant;
-// }
-
-// console.log(restaurants.fetchRestaurants());
-// printRestaurant();
-console.log(fetchedRestaurant);
